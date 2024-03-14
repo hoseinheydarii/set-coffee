6a847b5c17f74b49a41cd111b7da8e954570261c
unknown <hosein>
2024-03-14 11:59:30 +0330

    Banner component

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import Navbar from "@/components/modules/navbar/Navbar";
+import Banner from "@/components/templates/index/banner/Banner";
 import React from "react";
 
 const Home = () => {
   return (
     <>
       <Navbar />
+      <Banner />
     </>
   );
 };
